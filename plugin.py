b0488eba3bf12ff031c182d22fa74ecf62a504ee
benzrf <benzrf@benzrf.com>
2016-03-08 19:40:48 -0500

    methods that do the work

--- plugin.py
+++ plugin.py
@@ -49,6 +49,39 @@ class Lispnoria(callbacks.Plugin):
         with open(filename, 'w') as f:
             yaml.dump(self.lisp_env, f, parthial.serialize.ParthialDumper)
 
+    def _lispparse(self, irc, code, mod=parse.botlisp_expr):
+        try:
+            return parse.parse(code, mod)
+        except parse.NoLex as e:
+            msg, char = e.args
+            irc.error("lex error at char {}: {}".format(char, msg), Raise=True)
+        except parse.NoParse as e:
+            msg, char = e.args
+            if char:
+                irc.error("parse error at char {}: {}".format(char, msg),
+                    Raise=True)
+            else:
+                irc.error("parse error: {}".format(msg), Raise=True)
+
+    def _lispinterpret(self, irc, msg, expr, respond=False):
+        try:
+            env = self.lisp_env.new_child()
+            p = parthial.context.Context(env)
+            p.bot_ctx = (self, irc.irc, msg)
+            res = p.eval(expr)
+
+            if not respond: return res
+            if isinstance(res, parthial.vals.LispSymbol):
+                res = res.val
+            else:
+                res = str(res)
+            if res:
+                irc.reply(res)
+        except parthial.errs.LimitationError as e:
+            irc.error("killed: {}".format(e.message()), Raise=True)
+        except parthial.errs.LispError as e:
+            irc.error(e.message(), Raise=True)
+
 
 Class = Lispnoria
 
